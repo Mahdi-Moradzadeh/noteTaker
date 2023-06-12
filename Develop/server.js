a16d2b636012684e6028fd051edb9eb0c051f142
Mohammadmahdi Moradzadeh <126528880+Mahdi-Moradzadeh@users.noreply.github.com>
2023-06-12 17:27:19 -0400

    Server created, routes defined and needs to store

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const notes = require("./db/db.json");
 const PORT = 3001;
 
 app.use(express.static('public'));
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
 
-app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+    res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    notes.push(newNote);
+    res.json(newNote);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    const id = req.params.id;
+    notes.splice(id, 1);
+    res.json(notes);
+});
+
+app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "db/db.json")));
+
+
 
 app.listen(PORT, () => console.log(`App listening on PORT http://localhost:${PORT}`));
